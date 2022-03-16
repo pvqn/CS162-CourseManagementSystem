ee265c515620815f42b748779bef614b405a9c18
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2022-03-16 15:36:45 +0700

    Thêm class và account
    Có comment trong code, classes.cpp và user.cpp

--- MoodleFake/MoodleFake/classes.cpp
+++ MoodleFake/MoodleFake/classes.cpp
@@ -1,9 +1,16 @@
-#include "classes.h"
+﻿#include "classes.h"
+#include "user.h"
 #include <iostream>
 #include <fstream>
+#include <direct.h>
 
 using namespace std;
 
 void createClass(string className) {
-	
+	string path = "data/classes/" + className; // Tạo file class, trong class chứa info học sinh của class đó
+	mkdir(path.c_str()); // Tạo thư mục theo đường dẫn path
+	ofstream out;
+	out.open("data/classes/classList.txt", ios::app); // File classList chứa toàn bộ các class đã tạo (chỉ chứa tên)
+	out << className << '\n';
+	out.close();
 }
--- MoodleFake/MoodleFake/user.cpp
+++ MoodleFake/MoodleFake/user.cpp
@@ -1,1 +1,19 @@
 #include "user.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//void createAcc(User* head) { //create new user
+//	User* tmp = head->next;
+//	head->next = new User;
+//	head->next->next = tmp;
+//}
+
+void addAcc(User acc) {
+	ofstream out;
+	out.open("data/accounts.txt", ios::app);
+	out << acc.username << ' ' << acc.password << ' '
+		<< acc.role << ' ' << acc.Class << '\n'; //In thông tin user ra file account.txt
+	out.close();
+}
