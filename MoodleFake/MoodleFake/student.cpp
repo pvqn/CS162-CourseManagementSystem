2758560254c637a8151aa06a9f2106d95a97c2ef
hoktro <94552405+hoktro@users.noreply.github.com>
2022-03-18 01:30:50 +0700

    View student in a class
    ClassList is still not exist

--- MoodleFake/MoodleFake/student.cpp
+++ MoodleFake/MoodleFake/student.cpp
@@ -1,1 +1,21 @@
 #include "student.h"
+#include "classes.h"
+void ViewClass( Class *ClassList ) // view HS trong lop
+{
+    string Name;
+    cout << "Enter class name: "; cin >> Name;
+    Class *cur = ClassList;
+    while( cur && cur->ClassName != Name ) cur = cur->next;
+    if( !cur )
+    {
+        cout << "The class is not exist.";
+        return;
+    }
+    Student *Person = cur->StudentList;
+    int index = 0;
+    while( Person )
+    {
+        cout << (++index) << ". " << Person->firstName << ' ' << Person->lastName << '\n';
+        Person = Person->next;
+    }
+}
