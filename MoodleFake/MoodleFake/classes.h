3539c00639d8f56b10feaa3201116137e05ed343
hoktro <94552405+hoktro@users.noreply.github.com>
2022-03-18 01:28:35 +0700

    Create struct "Class"

--- MoodleFake/MoodleFake/classes.h
+++ MoodleFake/MoodleFake/classes.h
@@ -3,5 +3,12 @@
 #include "student.h"
 using namespace std;
 
+struct Class
+{
+    Class *next;                   // Create "ClassList" for searching
+    Student *StudentList;           // Create "StudentList"
+    string ClassName;
+};
+
 void createClass(string className);
 
