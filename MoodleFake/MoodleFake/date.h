7c5a55d89c6613a8640850f5a47dc7291da4f3da
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2022-03-18 15:11:06 +0700

    Commit cho trống thôi

--- MoodleFake/MoodleFake/date.h
+++ MoodleFake/MoodleFake/date.h
@@ -8,5 +8,5 @@ struct Date {
 	Date(int day = -1, int month = -1, int year = -1) :
 		day(day), month(month), year(year)
 	{};
-};
 
+};
--- MoodleFake/MoodleFake/display.h
+++ MoodleFake/MoodleFake/display.h
@@ -1,3 +1,4 @@
 #pragma once
 void gotoxy(short x, short y);
 void SetColor(int backgound_color, int text_color);
+
