cf626c42adee04f108d7817f5f4ddd45d7040c1e
pvqn <phamvoquynhnhu24@gmail.com>
2022-03-18 12:17:47 +0700

    - Fix lại function viewcourse_student

--- MoodleFake/MoodleFake/course.h
+++ MoodleFake/MoodleFake/course.h
@@ -4,11 +4,16 @@
 
 #include "mark.h"
 
-
 using namespace std;
 
-struct Course {
-	Course* next;
+struct student_list
+{
+	string id;
+	Mark mark;
+	student_list* next = nullptr;
+};
+struct Course 
+{
 	string id;
 	string name;
 	string teacher;
@@ -16,6 +21,8 @@ struct Course {
 	int maxCapacity;
 	string day1, ses1;
 	string day2, ses2;
+	student_list* student = nullptr;
+	Course* next = nullptr;
 };
 
 //Không cần tạo student list vì muốn search student trong 1 course thì dùng function search, chứ tạo nữa rối :v
--- MoodleFake/MoodleFake/semester.h
+++ MoodleFake/MoodleFake/semester.h
@@ -3,12 +3,16 @@
 #include <direct.h>
 #include <fstream>
 #include "date.h"
-
+#include "course.h"
 using namespace std;
 
 struct Semester {
 	int year, term;
-	Date* startDate,* endDate;
+	Date startDate, endDate;
+	// doi date * thanh date
+	Course* course_cur = nullptr;
+	Semester* next = nullptr;
+	
 };
 
 void setCurrentSemester(int year, int term, Date start, Date end);
