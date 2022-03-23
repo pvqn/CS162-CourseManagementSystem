60276f5b5d16a1e71654eda09880a9eba94df805
tron <kidopro03@gmail.com>
2022-03-24 01:08:31 +0700

    Task 8: Add a Course

--- MoodleFake/MoodleFake/course.cpp
+++ MoodleFake/MoodleFake/course.cpp
@@ -1,3 +1,28 @@
 #include "struct.h"
 
+void addCourse(Course* pCourse, Course* newCourse)
+{
+	Course* pCur = pCourse;
+
+	while (pCur != nullptr)
+	{
+		if (pCur->id == newCourse->id)
+		{
+			cout << "Failed to add a new course!!\n";
+			cout << "The course you want to add has already existed!!\n";
+			system("pause");
+			system("cls");
+			return;
+		}
+		pCur = pCur->next;
+	}
+	if (pCourse == nullptr)
+	{
+		pCourse = newCourse;
+		return;
+	}
+	pCur->next = newCourse;
+	pCur = pCur->next;
+}
+
 
--- MoodleFake/MoodleFake/course.h
+++ MoodleFake/MoodleFake/course.h
@@ -4,4 +4,5 @@
 using namespace std;
 
 
+void addCourse(Course* pCourse, Course* newCourse);
 
