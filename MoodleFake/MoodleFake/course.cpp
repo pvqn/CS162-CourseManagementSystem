fb96800df222dd999779074d68fd26a67c5a3c60
tron <kidopro03@gmail.com>
2022-03-24 14:20:26 +0700

    Task 9: View Course (chưa mã hoa)

--- MoodleFake/MoodleFake/course.cpp
+++ MoodleFake/MoodleFake/course.cpp
@@ -25,4 +25,21 @@ void addCourse(Course* pCourse, Course* newCourse)
 	pCur = pCur->next;
 }
 
+void view_Course(Course* pCur, Semester* Scur)
+{
+	string path = "data/" + to_string(Scur->year) + "/" + to_string(Scur->term) + "/courses/courseList.txt";
+	ofstream fout(path);
+	while (pCur != nullptr)
+	{
+		fout << pCur->id << endl;
+		fout << pCur->name << endl;
+		fout << pCur->teacher << endl;
+		fout << pCur->nCredits << endl;
+		fout << pCur->maxCapacity << endl;
+		fout << pCur->day1 << " : " << pCur->ses1 << endl;
+		fout << pCur->day2 << " : " << pCur->ses2 << endl;
+		pCur = pCur->next;
+	}
+	fout.close();
+}
 
