20ab33e68815989409d0e22489269639f609532d
tron <kidopro03@gmail.com>
2022-03-25 10:43:59 +0700

    Update file semester.h

--- MoodleFake/MoodleFake/semester.h
+++ MoodleFake/MoodleFake/semester.h
@@ -6,6 +6,8 @@ using namespace std;
 
 void setCurrentSemester(int year, int term, Date start, Date end);
 Semester getCurrentSemester();
-void Create_newSemester(Semester* Scur, int year);
+void Create_newSemester(Semester* Scur);
+void CourseRegister(Date& start, Date& end, int semester);
+
 
 
