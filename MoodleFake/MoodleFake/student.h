5b83ddcf5b498bf34911dbc60ca79ce057ac681b
hoktro <hoktro18@gmail.com>
2022-04-01 18:08:50 +0700

    Complete student function

--- MoodleFake/MoodleFake/student.h
+++ MoodleFake/MoodleFake/student.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 
+void displayclass_student(); // display class students
+void displaycourse_list(); // In ra danh sach cac khoa hoc
+void displaycourse_studentList();
 
 
 
