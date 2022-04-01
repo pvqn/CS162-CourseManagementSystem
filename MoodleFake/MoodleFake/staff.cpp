610a1cd8a4fcd4e19554ee0e3919f73214724049
hoktro <hoktro18@gmail.com>
2022-04-01 18:09:52 +0700

    Export and import scoreboard for course

--- MoodleFake/MoodleFake/staff.cpp
+++ MoodleFake/MoodleFake/staff.cpp
@@ -1,5 +1,6 @@
 #include "schoolYear.h"
 #include "semester.h"
+
 void createSchoolYear() {
 	int year;
 	cout << "Year(yyyy): ";
@@ -63,4 +64,123 @@ void addStudentFromCSV(string className,string courseID, Student*& student) {
 		}
 	}
 	fin.close();
+}
+
+void ExportCourseStudentList()
+{
+	//  Enter the year and the semester
+	int Year = 0, Term = 0;
+	cout << "Please input the year: "; cin >> Year;
+	cout << "Please input the semester: "; cin >> Term;
+
+	// Print student in this course
+	string Course_name;
+	cout << "Please enter course name ( Example CS162 ): "; cin >> Course_name;
+	string path = "data/" + to_string(Year) + '/' + to_string(Term) + "/courseList.txt";
+	bool Check = false;
+
+	ifstream fin;
+	ofstream fout;
+
+	fin.open(path);
+	while (!fin.eof())
+	{
+		string cur; fin >> cur;
+		if (cur == Course_name) Check = true;
+	}
+	fin.close();
+	if (!Check)
+	{
+		cout << "Your finding course is not exist !";
+		return;
+	}
+
+	string path_in = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/studentList.txt";
+	string path_out = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/StudentList.csv";
+	string path_mark = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/mark.txt";
+	ifstream Mark; Mark.open(path_mark);
+	fin.open(path_in);
+	fout.open(path_out);
+	fout << "No,ID,Name,Midterm Mark,Final mark,Other mark,Total mark" << '\n';
+	int No = 0;
+	while (!fin.eof())
+	{
+		string ID, Name;
+		float Mid, Final, Other, Total;
+		string ID_mark;
+
+		fin >> ID;
+		fin.ignore(); getline(fin, Name);
+		Mark >> ID_mark >> Mid >> Final >> Other >> Total;
+		if (ID != ID_mark)
+		{
+			system("pause");
+			cout << "Error!!!!!!!!!!!!!!!!!!!!!!!!";
+		}
+		fout << ++No << ',' << ID << ',' << Name << ',' << Mid << ',' << Final << ',' << Other << ',' << Total << '\n';
+	}
+	fin.close();
+	fout.close();
+	Mark.close();
+}
+
+void ImportCourseScore()
+{
+	//  Enter the year and the semester
+	int Year = 0, Term = 0;
+	cout << "Please input the year: "; cin >> Year;
+	cout << "Please input the semester: "; cin >> Term;
+
+	// Print student in this course
+	ifstream fin; ofstream fout;
+	string Course_name;
+
+	cout << "Please enter course name ( Example CS162 ): "; cin >> Course_name;
+	string path = "data/" + to_string(Year) + '/' + to_string(Term) + "/courseList.txt";
+	bool Check = false;
+	fin.open(path);
+	while (!fin.eof())
+	{
+		string cur; fin >> cur;
+		if (cur == Course_name) Check = true;
+	}
+	fin.close();
+	if (!Check)
+	{
+		cout << "Your finding course is not exist !";
+		return;
+	}
+
+	string path_in = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/Teacher.csv";
+	string path_out = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/StudentList.csv";
+	fin.open(path_in); fout.open(path_out);
+	while (!fin.eof())
+	{
+		string current;
+		getline(fin, current);
+		fout << current << '\n';
+	}
+	fin.close(); fout.close();
+
+	path_in = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/StudentList.csv";
+	path_in = "data/" + to_string(Year) + '/' + to_string(Term) + "/" + Course_name + "/mark.txt";
+	fin.open(path_in); fout.open(path_out);
+	while (!fin.eof())
+	{
+		string current;
+		getline(fin, current);
+		int count = 0;
+		for (int i = 0; i < current.size(); ++i)
+		{
+			if (current[i] == ',')
+			{
+				++count;
+				if (count == 2 || count == 4 || count == 5 || count == 6 ) fout << '\n';
+				continue;
+			}
+			if (count == 1 || count == 3 || count == 4 || count == 5 || count == 6) fout << current[i];
+		}
+		fout << '\n';
+	}
+	fin.open(path_in); fout.open(path_out);
 }
